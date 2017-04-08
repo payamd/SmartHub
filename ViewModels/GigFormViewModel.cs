2c2d7c5c766631914d2a1287a502eb37a90fc925
payamd <payam.d7@gmail.com>
2017-04-08 12:37:28 +0430

    GigFormViewModel

---
+++ ViewModels/GigFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+ 
+    }
+}

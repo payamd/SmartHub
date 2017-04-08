073bae9ba0c465b295b904eac4b2266295857599
payamd <payam.d7@gmail.com>
2017-04-08 10:47:09 +0430

    create Gig Model

--- Models/Genre.cs
+++ Models/Genre.cs
@@ -1,8 +1,13 @@
-﻿namespace SmartHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartHub.Models
 {
     public class Genre
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }

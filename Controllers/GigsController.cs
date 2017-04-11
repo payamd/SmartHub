45a0f07d000fc7552c48cd04960582c31545c773
payamd <payam.d7@gmail.com>
2017-04-11 21:01:57 +0430

    change sting to date time in model :)

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -34,7 +34,7 @@ namespace SmartHub.Controllers
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = DateTime.Parse(string.Format("{0} {1}",ViewModel.Date ,ViewModel.Time )),
+                DateTime = ViewModel.DateTime,
                 GenreId = ViewModel.Genre,
                 Venue = ViewModel.Venue
             };
--- ViewModels/GigFormViewModel.cs
+++ ViewModels/GigFormViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using SmartHub.Models;
 
 namespace SmartHub.ViewModels
@@ -13,5 +11,11 @@ namespace SmartHub.ViewModels
         public string Time { get; set; }
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+        public DateTime DateTime {
+            get
+            {
+                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+            }
+        }
     }
 }

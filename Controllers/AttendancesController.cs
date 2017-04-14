1ffd7829bbb39e672a6dcbe4e96252b323fdd951
payamd <payam.d7@gmail.com>
2017-04-14 20:35:40 +0430

    API -- added to site

---
+++ Controllers/AttendancesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using SmartHub.Models;
+
+namespace SmartHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var UserId = User.Identity.GetUserId();
+
+            var exist = _context.Attendances.Any(a => a.AttendeeId == UserId && a.GigId == gigId);
+            if (exist)
+            {
+                return BadRequest("the gig is already selected");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = UserId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
